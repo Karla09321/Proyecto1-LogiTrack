46077ea36a389b375b93682fb206f2c84e6ea9de
karla09321 <karlalizcano36@gmail.com>
2025-11-28 12:07:29 -0500

    inicio

---
+++ src/main/java/com/logitrack/logitrack_system/LogitrackSystemApplication.java
@@ -0,0 +1,13 @@
+package com.logitrack.logitrack_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogitrackSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogitrackSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logitrack/logitrack_system/LogitrackSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logitrack.logitrack_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogitrackSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
